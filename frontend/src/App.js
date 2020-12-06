21587d570d785930c7df7badee5b54c92de0852a
Lexun <59420596+lexun1060@users.noreply.github.com>
2020-12-06 22:47:59 +0200

    realized HomeScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import {Container} from 'react-bootstrap'
+import HomeScreen from "./screen/HomeScreen";
+
 
 function App() {
-  return (
-    <div>
-      <h1>My shop</h1>
-    </div>
-  );
+    return (
+        <>
+            <Header/>
+            <main className="py-3">
+                <Container>
+                    <HomeScreen />
+                </Container>
+            </main>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
