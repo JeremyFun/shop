b7236eefa2e7daadcbce6ffe1d25ea13f38ac3e2
Lexun <59420596+lexun1060@users.noreply.github.com>
2020-12-07 11:36:08 +0200

    add ProductScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,23 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import {Container} from 'react-bootstrap'
 import HomeScreen from "./screen/HomeScreen";
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import ProductScreen from "./screen/ProductScreen";
 
 
 function App() {
     return (
         <>
-            <Header/>
-            <main className="py-3">
-                <Container>
-                    <HomeScreen />
-                </Container>
-            </main>
-            <Footer/>
+            <Router>
+                <Header/>
+                <main className="py-3">
+                    <Container>
+                        <Route path="/" component={HomeScreen} exact />
+                        <Route path="/product/:id" component={ProductScreen} exact />
+                    </Container>
+                </main>
+                <Footer/>
+            </Router>
         </>
     );
 }
