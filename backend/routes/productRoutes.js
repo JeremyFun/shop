3ca630d62e9ebe5900d44446831c389b4ea675f3
Lexun <59420596+lexun1060@users.noreply.github.com>
2020-12-11 11:24:17 +0200

    add middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -16,7 +16,14 @@ router.get('/', asyncHandler(async (req, res) => {
 // @access   Public
 router.get('/:id', asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
-    res.json(product)
+
+    if (product) {
+        res.json(product)
+    } else {
+        res.status(404)
+        throw new Error("Product not found")
+    }
+
 }))
 
 export default router
