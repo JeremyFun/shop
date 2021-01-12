02ae1b4eff3d637e57002a5f75cfb00ed4aaeb6e
Lexun <59420596+lexun1060@users.noreply.github.com>
2021-01-12 23:33:50 +0200

    prepare

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,7 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/build')))
+
     app.get('*', (req, res) =>
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
 } else {
@@ -50,5 +51,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(5000,
+app.listen(PORT,
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
